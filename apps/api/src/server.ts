bc2500436661edc670fb952af12ec31b6a214efe
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 19:04:41 +0530

    feat: integrate apollo graphql server with express

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,37 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running");
+const apolloServer = new ApolloServer({
+  typeDefs: `#graphql
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "Hello GraphQL",
+    },
+  },
 });
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
-});
+async function startServer() {
+  await apolloServer.start();
+
+  app.use("/graphql", expressMiddleware(apolloServer));
+
+  app.get("/", (req, res) => {
+    res.send("API running");
+  });
+
+  app.listen(4000, () => {
+    console.log("Server running on http://localhost:4000");
+    console.log("GraphQL at http://localhost:4000/graphql");
+  });
+}
+
+startServer();
