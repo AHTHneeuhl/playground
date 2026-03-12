279d1040c6def20bc5d3af7899a38688bfc88e8a
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 20:36:21 +0530

    feat: add createUser method using prisma

--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -4,4 +4,10 @@ import { PrismaService } from "../../prisma/prisma.service";
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
+  async createUser(email: string, name?: string) {
+    return this.prisma.user.create({
+      data: { email, name },
+    });
+  }
 }
