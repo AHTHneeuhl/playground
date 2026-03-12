d4df4e50e5bedbe0b377679900ba44d447a198a9
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 19:24:34 +0530

    feat: generate playground and diagram modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from './modules/user/user.module';
+import { PlaygroundModule } from './modules/playground/playground.module';
+import { DiagramModule } from './modules/diagram/diagram.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PlaygroundModule, DiagramModule],
   controllers: [],
   providers: [],
 })
