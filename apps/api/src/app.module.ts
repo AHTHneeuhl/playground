fcd1b2ae9a09d1e98ef08537ac5e34367c385c1f
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 19:29:44 +0530

    feat: configure graphql module with apollo driver

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
+
 import { UserModule } from "./modules/user/user.module";
 import { PlaygroundModule } from "./modules/playground/playground.module";
 import { DiagramModule } from "./modules/diagram/diagram.module";
 
 @Module({
-  imports: [UserModule, PlaygroundModule, DiagramModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+    }),
+    UserModule,
+    PlaygroundModule,
+    DiagramModule,
+  ],
 })
 export class AppModule {}
