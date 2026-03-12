09a813bc151aa5a8177fefd1545d86e4868581ff
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 21:00:41 +0530

    feat: implement user registration with hashed password

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../../prisma/prisma.service";
+import * as bcrypt from "bcrypt";
+import { RegisterDto } from "./dto/register.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(dto: RegisterDto) {
+    const hashed = await bcrypt.hash(dto.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: dto.name,
+        password: hashed,
+      },
+    });
+  }
+}
